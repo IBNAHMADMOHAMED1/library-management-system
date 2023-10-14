ed9b4f5c93cb3f94abac44d3267d6ffb6c0e4e3a
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-14 14:46:04 +0100

    new fields to book transaction

--- src/main/java/com/library/librarymanagementsystem/Service/Impl/BorrowingServiceImpl.java
+++ src/main/java/com/library/librarymanagementsystem/Service/Impl/BorrowingServiceImpl.java
@@ -1,8 +1,9 @@
-package com.library.librarymanagementsystem.Service;
+package com.library.librarymanagementsystem.Service.Impl;
 
 import com.library.librarymanagementsystem.Dto.BookDto;
 import com.library.librarymanagementsystem.Repository.BookTransactionRepository;
 import com.library.librarymanagementsystem.Repository.UserRepository;
+import com.library.librarymanagementsystem.Service.BorrowingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class BorrowingServiceImpl implements BorrowingService{
+public class BorrowingServiceImpl implements BorrowingService {
 
     private final BookTransactionRepository bookTransactionRepository ;
 
