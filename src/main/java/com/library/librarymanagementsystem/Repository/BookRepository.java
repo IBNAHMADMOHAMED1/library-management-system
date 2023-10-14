dc21c1796f2c7e3ffd9e162b674caf5557aaeb7e
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-14 16:58:45 +0100

    complete the borrowing functionality

--- src/main/java/com/library/librarymanagementsystem/Repository/BookRepository.java
+++ src/main/java/com/library/librarymanagementsystem/Repository/BookRepository.java
@@ -2,6 +2,10 @@ package com.library.librarymanagementsystem.Repository;
 
 import com.library.librarymanagementsystem.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Book b WHERE b.id = :bookId and b.status='AVAILABLE'")
+    boolean isAvailable(@Param("bookId") Long bookId);
 }
