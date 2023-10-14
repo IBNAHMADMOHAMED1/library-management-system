9ec78939bb7f46a5859fc325e2924fe3b6415925
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-14 16:43:36 +0100

    fix Ã

--- src/test/java/com/library/librarymanagementsystem/Service/Impl/BorrowingServiceImplTest.java
+++ src/test/java/com/library/librarymanagementsystem/Service/Impl/BorrowingServiceImplTest.java
@@ -1,4 +1,35 @@
+package com.library.librarymanagementsystem.Service.Impl;
+
+import com.library.librarymanagementsystem.Repository.BookTransactionRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BorrowingServiceImplTest {
-  
+
+    @Mock
+    BookTransactionRepository bookTransactionRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void borrowBook() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void canBorrowBook() {
+    }
 }
