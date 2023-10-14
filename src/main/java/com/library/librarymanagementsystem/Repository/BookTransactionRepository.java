ac75bbb34b0d0315874699de41e42d4e219cb8fe
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-14 14:03:31 +0100

    added book transaction mdomain

---
+++ src/main/java/com/library/librarymanagementsystem/Repository/BookTransactionRepository.java
@@ -0,0 +1,13 @@
+package com.library.librarymanagementsystem.Repository;
+
+import com.library.librarymanagementsystem.Entity.BookTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookTransactionRepository extends JpaRepository<BookTransaction,Long> {
+
+    @Query(value = "select count(id) from book_transactions where borrower_id =:memberId",nativeQuery = true)
+    Long countAllByUser(Long memberId);
+}
